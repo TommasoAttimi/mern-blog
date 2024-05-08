5427677cd5e5728372abf1f0e6932b0e19e8438d
Tommaso Tadiello <tommaso.attimi@pm.me>
2024-05-08 10:32:32 +0200

    complete sign up page UI and fixed flowbite tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,14 @@
+const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "node_modules/flowbite-react/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [flowbite.plugin()],
 };
