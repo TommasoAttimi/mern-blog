85228b37b9f1cc797eb7463235247341d740d169
Tommaso Tadiello <tommaso.attimi@pm.me>
2024-05-11 12:15:03 +0200

    add pre installation

--- server/index.js
+++ server/index.js
@@ -6,8 +6,8 @@ import userRoutes from "./routes/userRoute.js";
 import authRoutes from "./routes/authRoute.js";
 import postRoutes from "./routes/postRoute.js";
 import commentRoutes from "./routes/commentRoute.js";
-
 import cookieParser from "cookie-parser";
+import path from "path";
 
 const app = express();
 
@@ -27,10 +27,18 @@ app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
 
+const __dirname = path.resolve();
+
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 //middleware to handle errors
 app.use((err, req, res, next) => {
