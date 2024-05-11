1fd29e2f3faf2cbff2e25b797bcf6af9bee5602c
Tommaso Tadiello <tommaso.attimi@pm.me>
2024-05-11 13:36:36 +0200

    test render deploy

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import authRoutes from "./routes/authRoute.js";
 import postRoutes from "./routes/postRoute.js";
 import commentRoutes from "./routes/commentRoute.js";
 import cookieParser from "cookie-parser";
-import path from "path";
+// import path from "path";
 
 const app = express();
 
@@ -27,18 +27,18 @@ app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+// app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+// });
 
 //middleware to handle errors
 app.use((err, req, res, next) => {
